448244ade2ac455dbb517ad5c48d494ff704bd96
Stream Protocol <105049869+stream-protocol@users.noreply.github.com>
2023-08-28 13:39:44 +0300

    redirecting changes to fix no cookies error

--- apps/merchant-ui/src/components/Welcome.tsx
+++ apps/merchant-ui/src/components/Welcome.tsx
@@ -5,6 +5,7 @@ import { StreamPayMark } from './StreamPayMark';
 interface Props {
     className?: string;
     isBlocked: string;
+    isLoggedIn: string;
 }
 
 export function Welcome(props: Props) {
@@ -12,11 +13,17 @@ export function Welcome(props: Props) {
         <article className={props.className}>
             <StreamPayMark className="h-8 md:h-14 mb-10 md:mb-14" />
             <h1 className="text-black font-semibold text-2xl md:text-3xl mt-6">
-                Welcome to the StreamPayments Merchant Portal
+                Welcome to the StreamPay Merchant Portal
             </h1>
             <p className="mt-3 text-neutral-600">
-                StreamPay makes it easy for you to accept Stream Token, Solana and USDC payments on your Shopify site.
+                StreamPay makes it easy for you to accept Solana and USDC payments on your Shopify site.
             </p>
+            {props.isLoggedIn === 'false' && (
+                <div className="border border-orange-500 bg-orange-50 text-orange-800 rounded-lg p-4 flex flex-row items-center space-x-2 mt-10">
+                    <WarningAlt size={20} />
+                    <p>Please log in to access features</p>
+                </div>
+            )}
             {props.isBlocked === 'true' ? (
                 <div className="border border-orange-500 bg-orange-50 text-orange-800 rounded-lg p-4 flex flex-row items-center space-x-2 mt-10">
                     <WarningAlt size={20} />
